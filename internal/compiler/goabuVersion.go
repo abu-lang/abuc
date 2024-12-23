f169c201b523efd5359af4707e0faf4e8b49472a
Massimo Comuzzo <comuzzo.massimo@spes.uniud.it>
2024-12-23 21:22:44 +0100

    Bump goabu from 0.5.0 to 0.5.1

--- internal/compiler/goabuVersion.go
+++ internal/compiler/goabuVersion.go
@@ -6,4 +6,4 @@ package compiler
 // goabuVersion identifies the version of GoAbU used by abuc in the
 // compilation of the generated Go sources. It is either a string of the
 // type "v<major>.<minor>.<patch>" or the SHA identifier of a git commit.
-const goabuVersion = "v0.5.0"
+const goabuVersion = "v0.5.1"
